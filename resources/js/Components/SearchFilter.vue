ed8db49253da79a72904d18ab5c554cb2639d24f
Sam Hernandez <samd.hernandezc24@gmail.com>
2023-02-12 01:13:39 -0600

    adding button component global

--- resources/js/Components/SearchFilter.vue
+++ resources/js/Components/SearchFilter.vue
@@ -1,7 +1,7 @@
 <script setup>
 import Dropdown from '@/Components/Dropdown.vue'
 
-const emits = defineEmits(['update:modelValue', 'reset'])
+defineEmits(['update:modelValue', 'reset'])
 
 defineProps({
   modelValue: String,
@@ -30,13 +30,14 @@ defineProps({
           </div>
         </template>
       </Dropdown>
-      <label for="table-search" class="sr-only">Buscar</label>
-      <div class="relative mt-1">
+      <label for="table-search-users" class="sr-only">search</label>
+      <div class="relative">
         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-          <svg class="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" /></svg>
+          <svg class="w-5 h-5 fill-gray-700" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" /></svg>
         </div>
-        <input id="table-search" type="text" class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500" />
+        <input id="table-search-users" type="text" :value="modelValue" class="block py-3 pl-10 w-full border border-gray-50 rounded-r focus:ring-blue-500 focus:border-blue-500" autocomplete="off" name="search" placeholder="Buscar" @input="$emit('update:modelValue', $event.target.value)" />
       </div>
+      <button class="flex items-center justify-center mx-auto text-gray-500 hover:text-gray-700 focus:text-yellow-500 text-sm" type="button" @click="$emit('reset')">Reiniciar</button>
     </div>
   </div>
 </template>
--- resources/js/Pages/Usuarios.vue
+++ resources/js/Pages/Usuarios.vue
@@ -40,6 +40,10 @@ const select = () => {
           <option value="only">Solo Eliminado</option>
         </select>
       </SearchFilter>
+      <Link class="btn-yellow" href="/usuarios/create">
+        <span>Añadir</span>
+        <span class="hidden md:inline">&nbsp;Usuario</span>
+      </Link>
     </div>
     <div class="bg-white rounded-md shadow overflow-x-auto">
       <table class="w-full whitespace-nowrap">
