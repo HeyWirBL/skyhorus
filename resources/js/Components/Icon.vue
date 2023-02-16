9736add35c9aef76b0cf0b07269454022600e551
Sam Hernandez <samd.hernandezc24@gmail.com>
2023-02-15 18:20:46 -0600

    implementing operations with pozos model

--- resources/js/Components/Icon.vue
+++ resources/js/Components/Icon.vue
@@ -79,4 +79,10 @@ defineProps({ name: String })
   <svg v-else-if="name === 'three-dots'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
     <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
   </svg>
+  <svg v-else-if="name === 'paper-clip'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+    <path fill-rule="evenodd" d="M15.621 4.379a3 3 0 00-4.242 0l-7 7a3 3 0 004.241 4.243h.001l.497-.5a.75.75 0 011.064 1.057l-.498.501-.002.002a4.5 4.5 0 01-6.364-6.364l7-7a4.5 4.5 0 016.368 6.36l-3.455 3.553A2.625 2.625 0 119.52 9.52l3.45-3.451a.75.75 0 111.061 1.06l-3.45 3.451a1.125 1.125 0 001.587 1.595l3.454-3.553a3 3 0 000-4.242z" clip-rule="evenodd" />
+  </svg>
+  <svg v-else-if="name === 'pencil'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+  </svg>
 </template>
--- resources/js/Pages/Pozos/Show.vue
+++ resources/js/Pages/Pozos/Show.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { Head } from '@inertiajs/vue3'
+import { Head, Link } from '@inertiajs/vue3'
+import Icon from '@/Components/Icon.vue'
 
 defineProps({
   pozo: Object,
@@ -7,6 +8,107 @@ defineProps({
 </script>
 
 <template>
-  <Head :title="pozo.nombre_pozo" />
-  <h1>Pozo</h1>
+  <div>
+    <Head :title="`${pozo.nombre_pozo}`" />
+    <div class="flex items-center justify-start mb-8 w-full">
+      <h1 class="text-3xl font-bold">
+        <Link class="text-yellow-400 hover:text-yellow-600" href="/pozos">Pozos</Link>
+        <span class="text-yellow-400 font-medium">&nbsp;/</span> {{ pozo.nombre_pozo }}
+      </h1>
+      <Link class="btn-yellow ml-auto" :href="`/pozos/${pozo.id}/editar`">
+        <span>Editar</span>
+        <span class="hidden md:inline">&nbsp;Pozo</span>
+      </Link>
+    </div>
+    <div class="overflow-hidden bg-white shadow sm:rounded-lg">
+      <div class="px-4 py-5 sm:px-6">
+        <h3 class="text-lg font-medium leading-6 text-gray-900">Datos del muestreo</h3>
+        <p class="mt-1 max-w-2xl text-sm text-gray-500">Detalles del pozo.</p>
+      </div>
+      <div class="border-t border-gray-200">
+        <dl>
+          <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">Pozo/Instalación</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ pozo.nombre_pozo }}</dd>
+          </div>
+          <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">Punto de muestreo</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ pozo.punto_muestreo }}</dd>
+          </div>
+          <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">Fecha del muestreo</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ pozo.fecha_hora }}</dd>
+          </div>
+          <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">Presión</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ pozo.presion_kgcm2 }} KM/CM2 {{ pozo.presion_psi }} PSIA</dd>
+          </div>
+          <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">Temperatura</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ pozo.temp_C }} °C {{ pozo.temp_F }} °F</dd>
+          </div>
+          <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">Volúmen</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ pozo.volumen_cm3 }} CM3 {{ pozo.volumen_lts }} LTS</dd>
+          </div>
+          <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">Identificador</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              {{ pozo.identificador }}
+            </dd>
+          </div>
+          <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">Observaciones</dt>
+            <dd v-if="pozo.observaciones" class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              {{ pozo.observaciones }}
+            </dd>
+            <dd v-else class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Aún no hay observaciones.</dd>
+          </div>
+          <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">Reportes</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+              <ul role="list" class="divide-y divide-gray-400 rounded-md border border-gray-400">
+                <li class="flex items-center justify-between py-3 pl-3 pr-4 text-sm">
+                  <div class="flex w-0 flex-1 items-center">
+                    <Icon class="h-5 w-5 flex-shrink-0 text-gray-400" name="pencil" aria-hidden="true" />
+                    <span class="ml-2 w-0 flex-1 truncate">Análisis</span>
+                  </div>
+                  <div class="ml-4 flex-shrink-0">
+                    <Link href="#" class="font-medium text-yellow-600 hover:text-yellow-500">Generar</Link>
+                  </div>
+                </li>
+                <li class="flex items-center justify-between py-3 pl-3 pr-4 text-sm">
+                  <div class="flex w-0 flex-1 items-center">
+                    <Icon class="h-5 w-5 flex-shrink-0 text-gray-400" name="chart" aria-hidden="true" />
+                    <span class="ml-2 w-0 flex-1 truncate">Componentes</span>
+                  </div>
+                  <div class="ml-4 flex-shrink-0">
+                    <Link href="#" class="font-medium text-yellow-600 hover:text-yellow-500">Generar</Link>
+                  </div>
+                </li>
+                <li class="flex items-center justify-between py-3 pl-3 pr-4 text-sm">
+                  <div class="flex w-0 flex-1 items-center">
+                    <Icon class="h-5 w-5 flex-shrink-0 text-gray-400" name="paper-clip" aria-hidden="true" />
+                    <span class="ml-2 w-0 flex-1 truncate">Cromatografía de Gas</span>
+                  </div>
+                  <div class="ml-4 flex-shrink-0">
+                    <Link href="#" class="font-medium text-yellow-600 hover:text-yellow-500">Generar</Link>
+                  </div>
+                </li>
+                <li class="flex items-center justify-between py-3 pl-3 pr-4 text-sm">
+                  <div class="flex w-0 flex-1 items-center">
+                    <Icon class="h-5 w-5 flex-shrink-0 text-gray-400" name="paper-clip" aria-hidden="true" />
+                    <span class="ml-2 w-0 flex-1 truncate">Cromatografía Líquida</span>
+                  </div>
+                  <div class="ml-4 flex-shrink-0">
+                    <Link href="#" class="font-medium text-yellow-600 hover:text-yellow-500">Generar</Link>
+                  </div>
+                </li>
+              </ul>
+            </dd>
+          </div>
+        </dl>
+      </div>
+    </div>
+  </div>
 </template>
