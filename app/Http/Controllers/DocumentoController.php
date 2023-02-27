0a5c938455b1fcad994fee4c678fb73da457d901
MiguelMtz-Tv <miguelayo128@gmail.com>
2023-02-26 22:16:19 -0600

    upload and download documents fixed

--- app/Http/Controllers/DocumentoController.php
+++ app/Http/Controllers/DocumentoController.php
@@ -106,7 +106,7 @@ class DocumentoController extends Controller
                 $fileRoute = time().$filename;
                 $filesize = $file->getSize();
                 $filetype = $file->getClientOriginalExtension();
-                $file->storeAs('', $fileRoute);
+                $file->storeAs('public/files/', $fileRoute);
                 $document = new Documento();
                 $document->documento = '{"name": "'.$fileRoute.'", "size": "'.$filesize.'", "type": "'.$filetype.'", "usrName": "'.$filename.'" }';
                 $document->directorio_id = $request->directorio_id;
@@ -120,8 +120,8 @@ class DocumentoController extends Controller
 
    public function download($document)
     {
-        if(Storage::disk('local')->exists($document)){
-            return Storage::disk('local')->download($document);
+        if(Storage::disk('public')->exists($document)){
+            return Storage::disk('public')->download($document);
         }else{
             return response('¡404! No se pudo encontrar este recurso. Si ves este mensaje, por favor contacta con un administrador. <br/> Powered by: Nerd Rage!', 404);
         }
